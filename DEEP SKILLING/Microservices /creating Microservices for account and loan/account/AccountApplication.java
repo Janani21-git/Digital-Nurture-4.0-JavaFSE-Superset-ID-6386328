77c92fecfa96a95cbf0197d1f0cbf6df9dd666d2
Janani21-git <njananijanani352@gmail.com>
2025-07-18 14:29:02 +0530

    AccountApplication.java
    week 5 task 1

--- DEEP SKILLING/Microservices /creating Microservices for account and loan/account/AccountApplication.java
+++ DEEP SKILLING/Microservices /creating Microservices for account and loan/account/AccountApplication.java
@@ -1,1 +1,14 @@
+package com.example.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.example.account", "com.cognizant.account.controller"})
+public class AccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+}
 
