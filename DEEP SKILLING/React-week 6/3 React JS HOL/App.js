18b7679d6e09f8fa9001386a341a4bfacf2818c2
Janani21-git <njananijanani352@gmail.com>
2025-07-27 16:18:19 +0530

    Update App.js

--- DEEP SKILLING/React-week 6/3 React JS HOL/App.js
+++ DEEP SKILLING/React-week 6/3 React JS HOL/App.js
@@ -1,1 +1,14 @@
 
+import React from 'react';
+import './App.css';
+import CalculatorScore from './Components/CalculatorScore';
+
+function App() {
+  return (
+    <div className="App">
+      <CalculatorScore Name="Rubesh" School="SRDKVVJC" Total={440} Number={5} />
+    </div>
+  );
+}
+
+export default App;
