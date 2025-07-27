afc526c8578d7e718d39198d2473d6090a471589
Janani21-git <njananijanani352@gmail.com>
2025-07-27 16:30:16 +0530

    Update App.js

--- DEEP SKILLING/React-week 6/5 React JS HOL/App.js
+++ DEEP SKILLING/React-week 6/5 React JS HOL/App.js
@@ -1,1 +1,25 @@
+import React from 'react';
+import CohortDetails from './components/CohortDetails';
+
+function App() {
+  return (
+    <div>
+      <h2>Cohort Dashboard</h2>
+      <CohortDetails
+        name="React Bootcamp"
+        status="ongoing"
+        startDate="2025-07-01"
+        endDate="2025-08-01"
+      />
+      <CohortDetails
+        name="Java Fundamentals"
+        status="completed"
+        startDate="2025-04-01"
+        endDate="2025-06-01"
+      />
+    </div>
+  );
+}
+
+export default App;
 
